a640c5874dd10ad7e3ce3c06b9a804cf4ce306f8
Welliton da Conceicao de Araujo <welliton.araujo@compasso.com.br>
2025-05-10 10:30:22 -0300

    feat: auth de login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,15 @@
 'use client';
 
 import './globals.css';
-import { SidebarProvider } from '@/context/SidebarContext';
-import { HeaderProvider } from '@/context/HeaderContext';
-import Sidebar from '@/componentes/Sidebar';
+import { AuthProvider } from '@/context/AuthContext';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className="h-full">
       <body className="h-full overflow-hidden">
-        <SidebarProvider>
-          <HeaderProvider>
-            <div className="flex h-full">
-              <Sidebar />
-              <main className="flex-1 overflow-y-auto">
-                {children}
-              </main>
-            </div>
-          </HeaderProvider>
-        </SidebarProvider>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
