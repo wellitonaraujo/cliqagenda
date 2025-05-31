3bb48d574a265f6b4533dce60c311fed696f2cdb
Welliton da Conceicao de Araujo <welliton.araujo@compasso.com.br>
2025-05-31 11:04:20 -0300

    feat: animacao na transaçao de pagamentos

--- src/app/(private)/(no-sidebar)/payment/page.tsx
+++ src/app/(private)/(no-sidebar)/payment/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import PaymentMethodSelector from './ui/PaymentMethodSelector';
+import { AnimatePresence, motion } from 'framer-motion';
 import { usePlanStore } from '@/app/store/usePlanStore';
 import { useCardForm } from './hooks/useCardForm';
 import { useRouter } from 'next/navigation';
@@ -19,8 +20,7 @@ export default function Payment() {
   function handlePayment(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const hasErrors = Object.values(errors).some(error => error);
-
+    const hasErrors = Object.values(errors).some((error) => error);
     if (hasErrors) {
       toast.warning('Por favor, corrija os erros antes de continuar.');
       return;
@@ -34,22 +34,46 @@ export default function Payment() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
-      <div className="w-full max-w-xl bg-white p-6 rounded border border-gray-100 shadow-xs">
+      <div className="w-full max-w-xl bg-white p-6 rounded border border-gray-100 shadow-xs relative overflow-hidden">
         <Header />
         <PaymentMethodSelector
           selected={paymentMethod}
           onSelect={setPaymentMethod}
         />
-        {paymentMethod === 'card' ? (
-          <CardForm
-            cardData={cardData}
-            errors={errors}
-            onChange={handleChange}
-            onSubmit={handlePayment}
-          />
-        ) : (
-          <PixPayment />
-        )}
+
+        {/* Animação entre layouts */}
+        <div className="relative min-h-[600px]">
+          <AnimatePresence mode="wait">
+            {paymentMethod === 'card' ? (
+              <motion.div
+                key="card"
+                initial={{ opacity: 0, x: 40 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -40 }}
+                transition={{ duration: 0.3 }}
+                className="absolute top-0 left-0 w-full"
+              >
+                <CardForm
+                  cardData={cardData}
+                  errors={errors}
+                  onChange={handleChange}
+                  onSubmit={handlePayment}
+                />
+              </motion.div>
+            ) : (
+              <motion.div
+                key="pix"
+                initial={{ opacity: 0, x: -40 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 40 }}
+                transition={{ duration: 0.3 }}
+                className="absolute top-0 left-0 w-full"
+              >
+                <PixPayment />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
