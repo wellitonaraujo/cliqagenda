84d24852d47fcc788d897f691f8c124c73ded0e8
Welliton da Conceicao de Araujo <welliton.araujo@compasso.com.br>
2025-06-07 18:53:39 -0300

    fix: servicos de prod

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cliqagenda-api-production.up.railway.app',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true, 
 });
 
