8884ad91f4bd9daced4c10830c62ba3308e0da9b
Welliton da Conceicao de Araujo <welliton.araujo@compasso.com.br>
2025-06-07 17:24:56 -0300

    feat: add ambiente de dev

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
