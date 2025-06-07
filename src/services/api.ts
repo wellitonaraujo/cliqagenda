2002fb3b637c95ce407c97788e07a9b34f4ec990
Welliton da Conceicao de Araujo <welliton.araujo@compasso.com.br>
2025-06-07 19:29:21 -0300

    feat: sem variaveis

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: 'https://cliqagenda-api-production.up.railway.app',
   withCredentials: true, 
 });
+
+console.log('API baseURL:', process.env.NEXT_PUBLIC_API_BASE_URL);
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
